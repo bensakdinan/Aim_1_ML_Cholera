8be76d720b37544f6ec4910a8324b5a7acc266bf
Ben Sakdinan <143819318+bensakdinan@users.noreply.github.com>
2025-02-11 09:46:35 -0500

    Update xgboost_model.py
    Remove unused import statements

--- xgboost_model.py
+++ xgboost_model.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import DataOrganization
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import balanced_accuracy_score, roc_auc_score, make_scorer, accuracy_score
